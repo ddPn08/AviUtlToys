d295167d75cd1f8ff96d1e500d899afdb798ef84
ddPn08 <pyn.g.1125@gmail.com>
2022-06-01 15:11:21 +0900

    update: ファイル名でソート | files

--- plugins/common/client/toys/files/components/file-set-list.tsx
+++ plugins/common/client/toys/files/components/file-set-list.tsx
@@ -17,42 +17,44 @@ export const FileSetList: React.FC<{
   const [togglePending, setTogglePending] = useState(false)
   return (
     <Stack>
-      {fileSets.map((file, i) => (
-        <React.Fragment key={i}>
-          {i !== 0 && <Divider></Divider>}
-          <Flex
-            alignItems="center"
-            gap="1rem"
-            p="1rem"
-            bg={colorMode === 'dark' ? 'gray.700' : 'gray.200'}
-          >
-            <Text size="md" flex={1}>
-              {file.id}
-            </Text>
-            <IconButton
-              aria-label="edit"
-              onClick={() => {
-                navigate('./edit', { state: file })
-              }}
+      {fileSets
+        .sort((a, b) => a.id.toLowerCase().localeCompare(b.id.toLowerCase()))
+        .map((file, i) => (
+          <React.Fragment key={i}>
+            {i !== 0 && <Divider></Divider>}
+            <Flex
+              alignItems="center"
+              gap="1rem"
+              p="1rem"
+              bg={colorMode === 'dark' ? 'gray.700' : 'gray.200'}
             >
-              <EditIcon />
-            </IconButton>
-            <Switch
-              defaultChecked={!!file.enabled}
-              disabled={togglePending}
-              onChange={async (e) => {
-                setTogglePending(true)
-                await client.invoke(
-                  e.currentTarget.checked ? 'files:enable' : 'files:disable',
-                  file.id,
-                )
-                update()
-                setTogglePending(false)
-              }}
-            />
-          </Flex>
-        </React.Fragment>
-      ))}
+              <Text size="md" flex={1}>
+                {file.id}
+              </Text>
+              <IconButton
+                aria-label="edit"
+                onClick={() => {
+                  navigate('./edit', { state: file })
+                }}
+              >
+                <EditIcon />
+              </IconButton>
+              <Switch
+                defaultChecked={!!file.enabled}
+                disabled={togglePending}
+                onChange={async (e) => {
+                  setTogglePending(true)
+                  await client.invoke(
+                    e.currentTarget.checked ? 'files:enable' : 'files:disable',
+                    file.id,
+                  )
+                  update()
+                  setTogglePending(false)
+                }}
+              />
+            </Flex>
+          </React.Fragment>
+        ))}
     </Stack>
   )
 }
